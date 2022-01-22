ac73ca7fa3fd34b01c95f6a3c41e164099be2cf8
Akashreddy9876 <akashreddy.nandikonda0990@gmail.com>
2022-01-22 13:18:38 +0530

    Add endpoints for top shows and popular shows

--- src/main/java/com/example/rest/api/ImdbController.java
+++ src/main/java/com/example/rest/api/ImdbController.java
@@ -9,36 +9,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ImdbController {
+	//TODO checking the api
 	@RequestMapping("/")
 	public String initialise() {
 		return "API has been initialised...";
 	}
+	//TODO endpoint for top movies
 	@RequestMapping("/topmovies")
 	public ResponseEntity<Object> topmovies() throws IOException {
 		JsoupInit obj=new JsoupInit(); 
 		ArrayList<JSONObject> returnedMovies= obj.initialise_top_movies();
 		
 		JSONObject topmovies = new JSONObject();
-		topmovies.put("movies", returnedMovies);
+		topmovies.put("topmovies", returnedMovies);
 		
 		return new ResponseEntity<>(topmovies.toMap(), HttpStatus.OK);
 	}
-	@RequestMapping("/mostpopular")
-	public ResponseEntity<Object> mostpopular() throws IOException {
+	//TODO endpoint for most popular movies
+	@RequestMapping("/popularmovies")
+	public ResponseEntity<Object> popularmovies() throws IOException {
 		JsoupInit obj=new JsoupInit(); 
-		ArrayList<JSONObject> returnedMovies= obj.initialise_most_popular();
+		ArrayList<JSONObject> returnedMovies= obj.initialise_popular_movies();
 		JSONObject popularmovies = new JSONObject();
-		popularmovies.put("movies", returnedMovies);
+		popularmovies.put("popularmovies", returnedMovies);
 		
 		return new ResponseEntity<>(popularmovies.toMap(), HttpStatus.OK);
 	}
+	//TODO endpoint for top box office movies
 	@RequestMapping("/topboxoffice")
 	public ResponseEntity<Object> topboxoffice() throws IOException {
 		JsoupInit obj=new JsoupInit(); 
 		ArrayList<JSONObject> returnedMovies= obj.initialise_top_box_office();
 		JSONObject topbomovies = new JSONObject();
-		topbomovies.put("movies", returnedMovies);
+		topbomovies.put("topbomovies", returnedMovies);
 		
 		return new ResponseEntity<>(topbomovies.toMap(), HttpStatus.OK);
 	}
+	//TODO endpoint for top shows
+		@RequestMapping("/topshows")
+		public ResponseEntity<Object> topshows() throws IOException {
+			JsoupInit obj=new JsoupInit(); 
+			ArrayList<JSONObject> returnedshows= obj.initialise_top_shows();
+			
+			JSONObject topshows = new JSONObject();
+			topshows.put("topshows", returnedshows);
+			
+			return new ResponseEntity<>(topshows.toMap(), HttpStatus.OK);
+		}
+		//TODO endpoint for most popular movies
+		@RequestMapping("/popularshows")
+		public ResponseEntity<Object> popularshows() throws IOException {
+			JsoupInit obj=new JsoupInit(); 
+			ArrayList<JSONObject> returnedshows= obj.initialise_popular_shows();
+			JSONObject popularshows = new JSONObject();
+			popularshows.put("popularmovies", returnedshows);
+			
+			return new ResponseEntity<>(popularshows.toMap(), HttpStatus.OK);
+		}
+	
 }
--- src/main/java/com/example/rest/api/JsoupInit.java
+++ src/main/java/com/example/rest/api/JsoupInit.java
@@ -27,7 +27,7 @@ public class JsoupInit {
 		return list;
 	}
 	//TODO most popular movies
-	public ArrayList<JSONObject> initialise_most_popular() throws IOException{ 
+	public ArrayList<JSONObject> initialise_popular_movies() throws IOException{ 
 		ArrayList<JSONObject> list=new ArrayList<JSONObject>();
 		Document doc = Jsoup.connect("https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm").get();
 		Elements elements=doc.select("tbody.lister-list");
@@ -65,4 +65,43 @@ public class JsoupInit {
 		}
 		return list;
 	}
+	//TODO top 250 shows
+		public ArrayList<JSONObject> initialise_top_shows() throws IOException{ 
+			ArrayList<JSONObject> list=new ArrayList<JSONObject>();
+			Document doc = Jsoup.connect("https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250").get();
+			Elements elements=doc.select("tbody.lister-list");
+			for(Element e:elements.select("tr")) {
+				 var movie = new JSONObject();
+				 int rank = Integer.parseInt(e.select("td.titleColumn").text().split(" ")[0].replaceAll("\\p{Punct}", ""));
+				 int year = Integer.parseInt(e.select("td.titleColumn span.secondaryInfo").text().replaceAll("[\\[\\](){}]",""));
+				 float rating = Float.parseFloat(e.select("td.ratingColumn.imdbRating strong").text());
+				 String title = e.select("td.titleColumn a").text();
+				 movie.put("Rank", rank);
+				 movie.put("Title",title);
+				 movie.put("Year",year);
+				 movie.put("Rating",rating);
+				 list.add(movie);
+			}
+			return list;
+		}
+		//TODO most popular shows
+		public ArrayList<JSONObject> initialise_popular_shows() throws IOException{ 
+			ArrayList<JSONObject> list=new ArrayList<JSONObject>();
+			Document doc = Jsoup.connect("https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv").get();
+			Elements elements=doc.select("tbody.lister-list");
+			for(Element e:elements.select("tr")) {
+				 var movie = new JSONObject();
+				int rank = Integer.parseInt(e.select("td.titleColumn div.velocity").text().split(" ")[0]);
+				 int year = Integer.parseInt(e.select("td.titleColumn span.secondaryInfo").text().replaceAll("[()]", "").split(" ")[0]);
+				 String ratingg=e.select("td.imdbRating.ratingColumn strong").text().length()==0?"0":e.select("td.imdbRating.ratingColumn strong").text();
+				 Float rating=Float.parseFloat(ratingg);
+				 String title = e.select("td.titleColumn a").text();
+				 movie.put("Rank", rank);
+				 movie.put("Title",title);
+				 movie.put("Year",year);
+				 movie.put("Rating",rating);
+				 list.add(movie);
+			}
+			return list;
+		}
 }
